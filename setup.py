eb512ac9b5a6bdbfbc1f6911f7a5047a5497ff97
Erik van Blokland <erik@letterror.com>
2018-04-28 09:47:47 +0200

    Changes to setup.py to run in py3.

--- setup.py
+++ setup.py
@@ -6,31 +6,8 @@ from setuptools import setup
 try:
     import fontTools
 except:
-    print "*** Warning: defcon requires FontTools, see:"
-    print "    fonttools.sf.net"
-
-try:
-    import robofab
-except:
-    print "*** Warning: defcon requires RoboFab, see:"
-    print "    robofab.com"
-
-#if "sdist" in sys.argv:
-#    import os
-#    import subprocess
-#    import shutil
-#    docFolder = os.path.join(os.getcwd(), "documentation")
-#    # remove existing
-#    doctrees = os.path.join(docFolder, "build", "doctrees")
-#    if os.path.exists(doctrees):
-#        shutil.rmtree(doctrees)
-#    # compile
-#    p = subprocess.Popen(["make", "html"], cwd=docFolder)
-#    p.wait()
-#    # remove doctrees
-#    shutil.rmtree(doctrees)
-
-
+    print("*** Warning: defcon requires FontTools, see:")
+    print("    https://github.com/fonttools/fonttools")
 
 setup(name="defconAppKit",
     version="0.1",
